e43c6fbce2f955c877765ea73e8dabcf33e42fcc
Rusty Case <sdzekan@gmail.com>
2017-07-26 13:12:24 -0400

    Commit script to generate basic authentication strings

---
+++ GetBase64BasicCredentials.ps1
@@ -0,0 +1,25 @@
+﻿# -----------------------------------------------------------------------------
+#  .SYNOPSIS 
+# Generates a 'BASIC {encoded}' string for use as the value of the 
+# AUTHORIZATION header when sending http requests.
+#
+# .PARAMETER username 
+# The value of the username to encode.
+#
+# .PARAMETER password 
+# The value of the password to encode.
+# -----------------------------------------------------------------------------
+Function Get-Base64BasicCredentials
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$username,
+        [Parameter(Mandatory=$true)]
+        [string]$password
+    )
+
+    $toEncode = $username + ":" + $password
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($toEncode)
+    $encoded = [System.Convert]::ToBase64String($bytes)
+    return "BASIC " + $encoded
+}
